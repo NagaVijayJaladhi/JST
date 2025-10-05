42a46389474476af94908ddc32e75fec554ea376
Naga Vijay Jaladhi <nagavijay.j@outlook.com>
2025-10-05 15:56:53 +0530

    JST SQL Query

---
+++ jstdb.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2025 at 12:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jstdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `abbrivation`
+--
+-- Error reading structure for table jstdb.abbrivation: #1932 - Table &#039;jstdb.abbrivation&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.abbrivation: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`abbrivation`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cccinfo`
+--
+-- Error reading structure for table jstdb.cccinfo: #1932 - Table &#039;jstdb.cccinfo&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.cccinfo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`cccinfo`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gkmcq`
+--
+-- Error reading structure for table jstdb.gkmcq: #1932 - Table &#039;jstdb.gkmcq&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.gkmcq: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`gkmcq`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `indianacts`
+--
+-- Error reading structure for table jstdb.indianacts: #1932 - Table &#039;jstdb.indianacts&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.indianacts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`indianacts`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `indianhistory`
+--
+-- Error reading structure for table jstdb.indianhistory: #1932 - Table &#039;jstdb.indianhistory&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.indianhistory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`indianhistory`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `internationaldays`
+--
+-- Error reading structure for table jstdb.internationaldays: #1932 - Table &#039;jstdb.internationaldays&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.internationaldays: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`internationaldays`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `javafeatures`
+--
+-- Error reading structure for table jstdb.javafeatures: #1932 - Table &#039;jstdb.javafeatures&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.javafeatures: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`javafeatures`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `javatutorial`
+--
+-- Error reading structure for table jstdb.javatutorial: #1932 - Table &#039;jstdb.javatutorial&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.javatutorial: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`javatutorial`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `javaversions`
+--
+-- Error reading structure for table jstdb.javaversions: #1932 - Table &#039;jstdb.javaversions&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.javaversions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`javaversions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quotes`
+--
+-- Error reading structure for table jstdb.quotes: #1932 - Table &#039;jstdb.quotes&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.quotes: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`quotes`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `railwayinfo`
+--
+-- Error reading structure for table jstdb.railwayinfo: #1932 - Table &#039;jstdb.railwayinfo&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.railwayinfo: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`railwayinfo`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `selftest`
+--
+-- Error reading structure for table jstdb.selftest: #1932 - Table &#039;jstdb.selftest&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.selftest: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`selftest`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `socialmedia`
+--
+-- Error reading structure for table jstdb.socialmedia: #1932 - Table &#039;jstdb.socialmedia&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.socialmedia: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`socialmedia`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stateut`
+--
+-- Error reading structure for table jstdb.stateut: #1932 - Table &#039;jstdb.stateut&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.stateut: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`stateut`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+-- Error reading structure for table jstdb.student: #1932 - Table &#039;jstdb.student&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.student: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`student`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table jstdb.user: #1932 - Table &#039;jstdb.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`user`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worldhistory`
+--
+-- Error reading structure for table jstdb.worldhistory: #1932 - Table &#039;jstdb.worldhistory&#039; doesn&#039;t exist in engine
+-- Error reading data for table jstdb.worldhistory: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `jstdb`.`worldhistory`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
